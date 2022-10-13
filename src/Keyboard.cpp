dad68c6ba2312634dcc6ed8ef83601012a33d0aa
wendash <32465903+wendash@users.noreply.github.com>
2022-10-13 19:30:47 +0100

    Update Keyboard.cpp

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -208,7 +208,46 @@ size_t Keyboard_::write(const uint8_t *buffer, size_t size) {
 	}
 	return n;
 }
+size_t Keyboard_::pressSC(uint8_t k)
+{
+	uint8_t i;
 
+	// Add sc to the key report only if it's not already present
+	// and if there is an empty slot.
+	if (_keyReport.keys[0] != k && _keyReport.keys[1] != k &&
+		_keyReport.keys[2] != k && _keyReport.keys[3] != k &&
+		_keyReport.keys[4] != k && _keyReport.keys[5] != k) {
+
+		for (i=0; i<6; i++) {
+			if (_keyReport.keys[i] == 0x00) {
+				_keyReport.keys[i] = k;
+				break;
+			}
+		}
+		if (i == 6) {
+			setWriteError();
+			return 0;
+		}
+	}
+	sendReport(&_keyReport);
+	return 1;
+}
+
+size_t Keyboard_::releaseSC(uint8_t k)
+{
+	uint8_t i;
+
+	// Test the key report to see if k is present.  Clear it if it exists.
+	// Check all positions in case the key is present more than once (which it shouldn't be)
+	for (i=0; i<6; i++) {
+		if (0 != k && _keyReport.keys[i] == k) {
+			_keyReport.keys[i] = 0x00;
+		}
+	}
+
+	sendReport(&_keyReport);
+	return 1;
+}
 Keyboard_ Keyboard;
 
 #endif
